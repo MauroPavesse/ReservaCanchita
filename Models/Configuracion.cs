c2a2acc9af81565df605655da9de9767209f7c45
el peque <mauro-pavesse@hotmail.com>
2025-08-23 12:55:48 -0300

    logo

---
+++ Models/Configuracion.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaCanchita.Models;
+
+public class Configuracion
+{
+    public int Id { get; set; }
+    public string Campo { get; set; } = string.Empty;
+    public string ValorString { get; set; } = string.Empty;
+    public decimal ValorNumerico { get; set; }
+}
